23c7caa7b04d60d23eaa3f9cbd5507be6d3353d5
Ubuntu <ubuntu@ip-172-31-9-171.us-west-1.compute.internal>
2013-08-09 04:08:28 +0000

    Commiting readfili

--- web.js
+++ web.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
+var fs=require("fs");
+
+var buffer = fs.readFileSync("index.html", "utf8");
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
